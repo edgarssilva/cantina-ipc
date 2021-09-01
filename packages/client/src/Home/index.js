b15ff9450d01e3a5550e7aa671e6301dc91d1aca
Rodrigo Carreira <rodrigojdc@gmail.com>
2021-09-01 19:01:25 +0100

    Added IPCLogo to Homescreen

--- packages/client/src/Home/index.js
+++ packages/client/src/Home/index.js
@@ -1,20 +1,11 @@
-<<<<<<< HEAD
 import React from "react";
-import { Image, Text } from "react-native";
 import ipcLogo from "../../assets/104938.png";
 import Button from "../components/Button";
 import { Container, IpcLogo } from "./styles";
-=======
-import React from 'react';
-import { Text, Button } from 'react-native';
-
-import { Container } from './styles';
->>>>>>> b2f6cf521bd1a4b9d2859ea94f2ca279c1f2749a
 
 const Home = ({ navigation }) => {
   return (
     <Container>
-<<<<<<< HEAD
       <IpcLogo
         source={ipcLogo}
         style={{
@@ -24,15 +15,6 @@ const Home = ({ navigation }) => {
         }}
       />
       <Button text="Reservar" />
-=======
-      <Text>This is the Home Screen!!!</Text>
-      <Button
-        title='Go to Login'
-        onPress={() => {
-          navigation.navigate('Login');
-        }}
-      />
->>>>>>> b2f6cf521bd1a4b9d2859ea94f2ca279c1f2749a
     </Container>
   );
 };
