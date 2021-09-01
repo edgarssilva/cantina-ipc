56ab64339d5ca0c8d1e800f22d5f431dfd548af6
Edgar Silva <edgarasdaa10@gmail.com>
2021-09-01 18:52:59 +0100

    Changed header to custom component.

--- packages/client/App.js
+++ packages/client/App.js
@@ -4,13 +4,18 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Home from './src/Home';
 import Login from './src/Login';
+import MenuBar from './src/components/MenuBar';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator
+        screenOptions={{
+          headerTitle: (props) => <MenuBar {...props} />,
+        }}
+      >
         <Stack.Screen name='Home' component={Home} />
         <Stack.Screen name='Login' component={Login} />
       </Stack.Navigator>
---
+++ packages/client/src/components/MenuBar/styles.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-evenly;
+`;
+
+export const IPCLogo = styled.Image``;
