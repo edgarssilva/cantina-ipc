08c6a260e74372e6bedc0707b7b1491a2b860ea6
Edgar Silva <edgarasdaa10@gmail.com>
2021-09-01 19:21:55 +0100

    Added Logo to Header.

--- packages/client/src/components/MenuBar/index.js
+++ packages/client/src/components/MenuBar/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 
-import { Container } from './styles';
+import { Container, IPCLogo } from './styles';
+
+import IPCsource from '../../../assets/ipclogo.png';
 
 const MenuBar = () => {
   return (
     <Container>
       <Text>This is the title</Text>
+      <IPCLogo source={IPCsource} />
     </Container>
   );
 };
