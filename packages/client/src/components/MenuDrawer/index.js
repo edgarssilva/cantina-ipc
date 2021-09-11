607f53b2f0dd1cea6adaf4a5cfa26f10aef6922e
Rodrigo Carreira <rodrigojdc@gmail.com>
2021-09-11 11:39:54 +0100

    Create drawer component

---
+++ packages/client/src/components/MenuDrawer/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { View } from 'react-native';
+import { Container } from './styles';
+
+const MenuDrawer = () => {
+    return <View />;
+}
+
+export default MenuDrawer;
