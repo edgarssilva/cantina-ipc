5aafe275106fda61506f71ee6a57f2cb006b5d1d
Edgar Silva <edgarasdaa10@gmail.com>
2021-09-14 15:08:00 +0100

    Fix round button.

--- packages/client/src/components/Button/index.js
+++ packages/client/src/components/Button/index.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import { Container, ButtonContainer, ButtonText } from './styles';
 
-
-const Button = ({ text, onPress, fontSize, color, }) => {
-    return <Container>
-        <ButtonContainer onPress={onPress} >
-            <ButtonText fontSize={fontSize} color={color}>
-                {text}
-            </ButtonText>
-        </ButtonContainer>
-    </Container>;
-}
+const Button = ({ text, onPress, fontSize, color }) => {
+  return (
+    <Container>
+      <ButtonContainer onPress={onPress}>
+        <ButtonText fontSize={fontSize} color={color}>
+          {text}
+        </ButtonText>
+      </ButtonContainer>
+    </Container>
+  );
+};
 
 Button.defaultProps = {
-    text: "Reservar"
-}
+  text: 'Reservar',
+};
 
 export default Button;
